dd79f09baf0f34a9f679b8d69b67567ec200d956
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-03-18 15:17:01 +0330

    Change routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import './App.css';
-import signupPage from "./views/pages/signup.page";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import loginPage from "./views/pages/loginPage";
 
 function App() {
   return (
-    <div className="App">      
-      <Route path="/" exact component={signupPage}/>
+    <div className="App"> 
+      <Route path="/" exact component={loginPage}/>               
+      <Route path="/login" exact component={loginPage}/>      
     </div>
   );
 }
