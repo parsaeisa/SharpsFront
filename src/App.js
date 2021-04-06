c74acbf432d8323868371bbd23d99117c6c0e61d
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-04-06 17:27:51 +0430

    feature ( edit profile ) : start edit profile page by its routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import loginPage from "./views/pages/loginPage";
+import editProfile from "./views/pages/editProfile/editprofile" ;
 
 function App() {
   return (
     <div className="App"> 
       <Route path="/" exact component={loginPage}/>               
       <Route path="/login" exact component={loginPage}/>      
+      <Route path="/edit_profile" exact component={editProfile} />
     </div>
   );
 }
