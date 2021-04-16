6483b9c43f5b1ba53495a089155b3b06f2fcde14
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-04-16 10:31:25 +0430

    feat (edit profile) : set redux reducer , action and actiontype

---
+++ src/core/edit_profile/action/UserAction.js
@@ -0,0 +1,45 @@
+import {UserActionTypes} from './UserActionType' ;
+
+export function setFirstname (n){
+    return {
+        type : UserActionTypes.SET_FIRSTNAME ,
+        payload : { firstname : n }
+    }
+}
+
+export function setLastname (n){
+    return {
+        type : UserActionTypes.SET_LASTNAME ,
+        payload : { lastname : n }
+    }
+}
+
+export function setUsername (n){
+    return {
+        type : UserActionTypes.SET_USERNAME ,
+        payload : { username : n }
+    }
+}
+
+export function setPassword (n){
+    return {
+        type : UserActionTypes.SET_PASSWORD ,
+        payload : { password : n }
+    }
+}
+
+
+export function setEmail (n){
+    return {
+        type : UserActionTypes.SET_EMAIL ,
+        payload : { email : n }
+    }
+}
+
+
+export function setAvatar (n){
+    return {
+        type : UserActionTypes.SET_AVATAR ,
+        payload : { avatar : n }
+    }
+}
