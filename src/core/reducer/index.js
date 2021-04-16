e8f5f712ac032f4aee1b5e7402f144b055b86464
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-04-16 10:32:35 +0430

    feat ( edit profile) : add user reducer to store

--- src/core/reducer/index.js
+++ src/core/reducer/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 // import login_signup from "../login_signup/reducer/loginSignupReducer"
+import UserReducer from '../edit_profile/reducer/userReducer' ;
 
 export default combineReducers({
-    
+    UserReducer
 });
