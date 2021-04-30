5998574d89930d7e162aab5ec0453845620792a6
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-05-01 01:38:55 +0430

    feat ( analytics) : add bar chart and table sections , make code cleaner

--- src/views/pages/Analytics/Analytics.js
+++ src/views/pages/Analytics/Analytics.js
@@ -2,44 +2,28 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import Box from '@material-ui/core/Box';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import DonatChart from './components/Donat_chart';
 import LineChart from './components/Line_chart';
+import BarChart from './components/Bar_chart' ;
+import "../../styles/analytics.css";
+import BlockedTable from './components/blocked_links_table';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
   },    
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
   title: {
     flexGrow: 1,
   },  
-  appBarSpacer: theme.mixins.toolbar,
+  appBarSpacer:
+  {
+    height : '30px'
+  },
   content: {
     flexGrow: 1,
     height: '100vh',
@@ -80,18 +64,23 @@ export default function Analytics() {
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>            
             <Grid item xs={12} md={8} lg={8}>
-              <Paper className={fixedHeightPaper}>                
+              <Paper elevation={4} className={fixedHeightPaper}>                
                 <LineChart />
               </Paper>
             </Grid>            
             <Grid item xs={12} md={4} lg={4}>
-              <Paper className={fixedHeightPaper}>
+              <Paper elevation={4} className={fixedHeightPaper}>
                 <DonatChart />
               </Paper>
             </Grid>            
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>                
-                hellooooooooooo
+            <Grid item xs={12} md={4} lg={4}>
+              <Paper elevation={4} className={fixedHeightPaper}>
+                <BarChart />
+              </Paper>
+            </Grid>            
+            <Grid item xs={12} md={8} lg={8}>
+              <Paper elevation={4} className={classes.paper}>                
+                <BlockedTable />
               </Paper>
             </Grid>
           </Grid>          
