687c11c3ef6c076a8c843e3cbc0aeefbe07d0c44
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-05-02 01:18:43 +0430

    faet ( analytics ) : convert analytics from func to class

--- src/views/pages/Analytics/Analytics.js
+++ src/views/pages/Analytics/Analytics.js
@@ -7,85 +7,89 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import DonatChart from './components/Donat_chart';
 import LineChart from './components/Line_chart';
-import BarChart from './components/Bar_chart' ;
+import BarChart from './components/Bar_chart';
 import "../../styles/analytics.css";
 import BlockedTable from './components/blocked_links_table';
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },    
-  title: {
-    flexGrow: 1,
-  },  
-  appBarSpacer:
-  {
-    height : '30px'
-  },
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  container: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
-  fixedHeight: {
-    height: 300,
-  },
-}));
 
-export default function Analytics() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+export default class Analytics extends React.Component {
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      
-      <main className={classes.content}>
-        <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>            
-            <Grid item xs={12} md={8} lg={8}>
-              <Paper elevation={4} className={fixedHeightPaper}>                
-                <LineChart />
-              </Paper>
-            </Grid>            
-            <Grid item xs={12} md={4} lg={4}>
-              <Paper elevation={4} className={fixedHeightPaper}>
-                <DonatChart />
-              </Paper>
-            </Grid>            
-            <Grid item xs={12} md={4} lg={4}>
-              <Paper elevation={4} className={fixedHeightPaper}>
-                <BarChart />
-              </Paper>
-            </Grid>            
-            <Grid item xs={12} md={8} lg={8}>
-              <Paper elevation={4} className={classes.paper}>                
-                <BlockedTable />
-              </Paper>
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    const classes = makeStyles((theme) => ({
+      root: {
+        display: 'flex',
+      },
+      title: {
+        flexGrow: 1,
+      },
+      appBarSpacer:
+      {
+        height: '30px'
+      },
+      content: {
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+      },
+      container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+      },
+      paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+      },
+      fixedHeight: {
+        height: 300,
+      },
+    }));
+
+    const fixedHeightPaper = clsx( classes.paper, "fixedHeight");
+
+
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+
+        <main className="content" >
+          <div className="appBarSpacer"/>
+          <Container maxWidth="lg"  className="container">
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={8} lg={8}>
+                <Paper elevation={4} className={fixedHeightPaper}>
+                  <LineChart />
+                </Paper>
+              </Grid>
+              <Grid item xs={12} md={4} lg={4}>
+                <Paper elevation={4} className={fixedHeightPaper}>
+                  <DonatChart />
+                </Paper>
+              </Grid>
+              <Grid item xs={12} md={4} lg={4}>
+                <Paper elevation={4} >
+                  <BarChart />
+                </Paper>
+              </Grid>
+              <Grid item xs={12} md={8} lg={8}>
+                <Paper elevation={4} className="paper">
+                  <BlockedTable />
+                </Paper>
+              </Grid>
             </Grid>
-          </Grid>          
-        </Container>
-      </main>
-    </div>
-  );
+          </Container>
+        </main>
+      </div>
+    );
+  }
 }
