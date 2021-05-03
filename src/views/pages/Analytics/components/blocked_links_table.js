ea0dffae99142f9b0079cc3100eea5a8a068e8a1
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-05-03 22:49:01 +0430

    feat ( analytics ) : add raw api datas to table

--- src/views/pages/Analytics/components/blocked_links_table.js
+++ src/views/pages/Analytics/components/blocked_links_table.js
@@ -7,7 +7,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-
+import Typography from '@material-ui/core/Typography';
+import callapi_analytics_get_blockedDomains from '../callapi_analytics/callapi_analytics_blockdomains';
 
 export default class BlockedTable extends React.Component {
 
@@ -16,23 +17,30 @@ export default class BlockedTable extends React.Component {
     {
         super(props);
         this.state = {
-            blocked_links : [] ,
+            blocked_domains : [] ,
             blocked_topics : [] ,
         } ;
     }
     
-    componentWillMount()
+    async componentWillMount()
     {
         // request to back to get blocked links and topics 
+        let blocked_domains = await  callapi_analytics_get_blockedDomains() ;   
+        this.setState({            
+            blocked_domains : blocked_domains
+          })
     }
     
     render ()
     {
-        const createData = (name, fat) => {
-            return { name,  fat };
+        const createData = (name) => {
+            return { name};
         }
 
         const rows = [
+            // this.props.data.map((domain) => 
+            //     createData(domain)
+            // )
             createData('https://www.telerik.com/kendo-react-ui/components/charts/series-types/bar/#toc-100-stacked-bar', 159),
             createData('https://material-ui.com/components/tables/', 237),
             createData('https://trello.com/c/0f7ZPHGN/85-profile-analytics-features', 262 ),
@@ -46,28 +54,38 @@ export default class BlockedTable extends React.Component {
         });
 
         return (
-            <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                <TableRow>
-                    <TableCell> <b>Blocked Links </b></TableCell>
-                    {/* <TableCell align="right">Blocked Tags </TableCell>             */}
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {rows.map((row) => (
-                    <TableRow key={row.name}>
-                    <TableCell component="th" scope="row">
-                        <a href = {row.name}>
-                        {row.name}
-                        </a>
-                    </TableCell>              
-                    {/* <TableCell align="right">{row.fat}</TableCell> */}
+            <>
+            {this.state.blocked_domains.length != 0 ?
+                <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell> <b>Blocked Links </b></TableCell>
+                        {/* <TableCell align="right">Blocked Tags </TableCell>             */}
                     </TableRow>
-                ))}
-                </TableBody>
-            </Table>
-            </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                    {this.state.blocked_domains.map((row) => (
+                        <TableRow key={row.name}>
+                        <TableCell component="th" scope="row">
+                            <a href = {row.name}>
+                            {row.name}
+                            </a>
+                        </TableCell>              
+                        {/* <TableCell align="right">{row.fat}</TableCell> */}
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+                </TableContainer>
+            :
+                    <div>
+                         <Typography variant="h6" style = {{color : "#757575"}} >
+                            You haven't blocked any domain . 
+                        </Typography>
+                    </div>
+            }
+            </>
         );
     }
 }
