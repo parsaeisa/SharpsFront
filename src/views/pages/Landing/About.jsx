c2b4e338ad12a3a67f9c41a92c7db647f485e54f
hasti <hastikaramdel@gmail.com>
2021-05-12 21:19:54 +0430

    feature(style):landing page basic

---
+++ src/views/pages/Landing/About.jsx
@@ -0,0 +1,30 @@
+  
+export const About = (e) => {
+    return (
+      <div id='about'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-xs-12 col-md-6'>
+              {' '}
+              <img src='img/about.jpg' className='img-responsive' alt='' />{' '}
+            </div>
+            <div className='col-xs-12 col-md-6'>
+              <div className='about-text'>
+                <h2>About Us</h2>
+                
+                <h3>Why Choose Us?</h3>
+                <div className='list-style'>
+                  <div className='col-lg-6 col-sm-6 col-xs-12'>
+                   
+                  </div>
+                  <div className='col-lg-6 col-sm-6 col-xs-12'>
+                  
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
---
+++ src/views/pages/Landing/Feature.jsx
@@ -0,0 +1,14 @@
+export const Features = (e) => {
+    return (
+      <div id='features' className='text-center'>
+        <div className='container'>
+          <div className='col-md-10 col-md-offset-1 section-title'>
+            <h2>Features</h2>
+          </div>
+          <div className='row'>
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shopfactory.com%2Fcontents%2Fen-us%2Fd435_Features.html&psig=AOvVaw3ZuNEvTbYr44ADrfKkoOUH&ust=1620922328771000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPjh3bHExPACFQAAAAAdAAAAABAD"></img>
+          </div>
+        </div>
+      </div>
+    )
+  }
