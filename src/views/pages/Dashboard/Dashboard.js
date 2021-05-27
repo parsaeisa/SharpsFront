f583d1f516ec06888fe2c6a1810de2219219aecc
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-05-27 12:50:34 +0430

    feat ( responsive) : make dashboard responsive

--- src/views/pages/Dashboard/Dashboard.js
+++ src/views/pages/Dashboard/Dashboard.js
@@ -62,7 +62,7 @@ const useStyles = makeStyles((theme) => ({
       }),
     },
     appBarShift: {
-      marginLeft: drawerWidth,
+      // marginLeft: drawerWidth,
       width: `calc(100% - ${drawerWidth}px)`,
       transition: theme.transitions.create(['width', 'margin'], {
         easing: theme.transitions.easing.sharp,
@@ -81,7 +81,7 @@ const useStyles = makeStyles((theme) => ({
     drawerPaper: {
       position: 'relative',
       whiteSpace: 'nowrap',
-      width: drawerWidth,
+      // width: drawerWidth,
       transition: theme.transitions.create('width', {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.enteringScreen,
