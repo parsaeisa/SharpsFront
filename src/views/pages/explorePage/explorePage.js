4a38b1bc72c8a717aa01657ba927f33a4df9508b
SanaRayehi <sana.rayehi@gmail.com>
2021-05-31 03:34:27 +0430

    fix (explorePage) : fix footer of cards and position

--- src/views/pages/explorePage/explorePage.js
+++ src/views/pages/explorePage/explorePage.js
@@ -4,7 +4,7 @@ import { makeStyles, useTheme, fade } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import { useState, useEffect ,useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import '../../styles/explorePage.css';
 import serverURL from '../../../utils/serverURL';
 
@@ -30,9 +30,9 @@ const useStyles = makeStyles((theme) => ({
   },
 
 
-  
 
-  
+
+
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
@@ -86,10 +86,10 @@ function ExplorePage() {
   const theme = useTheme();
   const [open, setOpen] = useState(false);
   const [content, setContent] = useState({ items: [], total: "" });
-  const [hasMore, setHasMore] =useState(true);
-  const[ nextPage, SetNextPage] = useState(serverURL()+"user/suggestions?skip=0&limit=10&showAds=true");
-  const [value, setValue] =useState(0);
-  
+  const [hasMore, setHasMore] = useState(true);
+  const [nextPage, SetNextPage] = useState(serverURL() + "user/suggestions?skip=0&limit=10&showAds=true");
+  const [value, setValue] = useState(0);
+
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -101,13 +101,13 @@ function ExplorePage() {
 
   useEffect(async () => {
     fetchData()
-    
+
     // await timeout(20);
   }, []);
 
-  const fetchData = (value)=>{
-  
-  console.log(localStorage.getItem('token')+"hhhhhhhhhhhhhhh")
+  const fetchData = (value) => {
+
+    console.log(localStorage.getItem('token') + "hhhhhhhhhhhhhhh")
     fetch(nextPage, {
       method: 'GET',
       headers: {
@@ -115,13 +115,13 @@ function ExplorePage() {
       }
     })
       .then((result) => {
-         console.log("sanaaaaaaaaaaa")
+        console.log("sanaaaaaaaaaaa")
         if (result.status === 200) {
-        
-     
-        //   setValue(value+1)
-        // SetNextPage(serverURL()+"user/suggestions?skip="+value+"&limit=10&showAds=false");
-        return result.json()
+
+
+          //   setValue(value+1)
+          // SetNextPage(serverURL()+"user/suggestions?skip="+value+"&limit=10&showAds=false");
+          return result.json()
         }
 
         if (result.status === 401) {
@@ -136,65 +136,67 @@ function ExplorePage() {
           total: response.total
         }))
         // console.log("data")
-        setValue(value+10)
-        SetNextPage(serverURL()+"user/suggestions?skip="+value+"&limit=10&showAds=true");
+        setValue(value + 10)
+        SetNextPage(serverURL() + "user/suggestions?skip=" + value + "&limit=10&showAds=true");
       })
-      
+
       .then((response) => {
-       
-        // if(response.next === null){
-        //   setHasMore(false)
-        // }
       });
-   
-    }
- { console.log(content+"cont")}
- { console.log(value+"valueeee")}
+
+  }
+  { console.log(content + "cont") }
+  { console.log(value + "valueeee") }
   return (
     <div>
       <div className={classes.root}>
         <CssBaseline />
 
-        
-          {/* <div className={classes.drawerHeader} /> */}
-          <div className="explore">
 
-         <InfiniteScroll
-                scrollThreshold="90%"
-                dataLength={content.items.length}
+        {/* <div className={classes.drawerHeader} /> */}
+        <div className="explore">
 
-                next={fetchData()}
-                hasMore={hasMore}
-                loader={
-                //  <h4>Loading...</h4>
-                <div style={{width: "100%", justifyContent:"center", display:"flex"}}>
+          <InfiniteScroll
+            scrollThreshold="90%"
+            dataLength={content.items.length}
+
+            next={fetchData()}
+            hasMore={hasMore}
+            loader={
+              //  <h4>Loading...</h4>
+              <div style={{ width: "100%", justifyContent: "center", display: "flex" }}>
                 <div className="loading" />
-                </div>
+              </div>
             }
-                endMessage={ null
-                }>
+            endMessage={null
+            }>
             {content.length === 0 ? <div></div> :
               content.items.map((item) => {
                 if (item) return (
                   <div style={{ spacing: "50%" }}>
-                    <div class="card mb-3 " >
+                    <div class="card mb-3" >
                       <div class="row no-gutters">
-                      {/* <div className="backButton" style={{height:"60px", fontSize:"20px", paddingTop:"15px", color:"#e3e3e3"}}>
-                                        <a  href={"//" + item.url}  ><ArrowBackIcon /> </a>
-                                        </div> */}
                         <div class="col-md-4" >
                           <img variant="top" src={item.image != null ? item.image : "https://om.rosheta.com/upload/61e6aa724ce98c29726e423dd146e4bc9435f9ea5eca681a349a2e2ab0a23494.png"} rounded class="card-img" alt="..." ></img>
-                        
-  </div>
-                    <div class="col-md-8">
-                    <div className="reportwrapper" style={{ float: "right", display:"inline-block", marginTop:"9px",color:"black" }} >
-                    <Block url={item.url}  />
-                                            </div>
+                          <div class="card-footer w-100 text-muted" className=" horizontal-card-footer">
+                            <div style={{ display: "flex", content: "center", paddingLeft: "20px", marginTop: "" }}  >
+
+                              <SaveContent url={item.url} />
+                              <Like url={item.url} />
+
+                            </div>
+
+                          </div>
+
+                        </div>
+                        <div class="col-md-8">
+                          <div className="reportwrapper" style={{ float: "right", display: "inline-block", marginTop: "13px", color: "black" }} >
+                            <Block url={item.url} />
+                          </div>
                           <div class="card-body">
                             <h5 class="card-title">{item.title}</h5>
-                            <p class="card-text"  style={{ display:"inline",whiteSpace: "pre-line" }}>
-                             
-                            <ShowMoreText
+                            <p class="card-text" style={{ display: "inline", whiteSpace: "pre-line" }}>
+
+                              <ShowMoreText
                                 lines={2}
                                 more='Show more'
                                 less='Show less'
@@ -203,39 +205,29 @@ function ExplorePage() {
                                 className='content-css'
                                 anchorClass='my-anchor-css-class'
                                 expanded={false}
-                                keepNewLines = {true}
-                                  width={500}
-                            >
-                            {item.des} 
-                            </ShowMoreText>
-                               </p>
+                                keepNewLines={true}
+                                width={500}
+                              >
+                                {item.des}
+                              </ShowMoreText>
+                            </p>
                             <div>
-                            {/* <a href={"//" + item.url} class="stretched-link" /> */}
+                              {/* <a href={"//" + item.url} class="stretched-link" /> */}
                             </div>
-                          <div class="card-footer "style={{paddingBottom:"0%",paddingBottom:"10%" }} >
-                            <div style={{ display: "flex",content:"center",paddingLeft:"20px"}}  >
 
-<SaveContent url={item.url}  />
-<Like url={item.url}   />
-{/* <Block url={item.url}  /> */}
-          
-        </div>
-                           
-</div>
-
-</div>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 )
               })}
-              </InfiniteScroll>
+          </InfiniteScroll>
 
-          </div>
+        </div>
       </div>
 
-    </div>  
+    </div>
   );
 
 }
