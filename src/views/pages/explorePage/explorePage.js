19c1cc1e7e2f04579359a6a5442939fcc0b21350
SanaRayehi <sana.rayehi@gmail.com>
2021-05-31 13:37:40 +0430

    fix (explorePage) :  fix merge branch explorePage

--- src/views/pages/explorePage/explorePage.js
+++ src/views/pages/explorePage/explorePage.js
@@ -1,58 +1,24 @@
 import React from 'react';
 import clsx from 'clsx';
-import Button from '@material-ui/core/Button';
-import FilterListIcon from '@material-ui/icons/FilterList';
 import { makeStyles, useTheme, fade } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 import { useState, useEffect, useCallback } from "react";
-=======
-=======
-import { useState, useEffect, useCallback } from "react";
->>>>>>> 9aef2f55f5e98510450522dac6de000556692de1
-import AppBar from '@material-ui/core/AppBar';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
-<<<<<<< HEAD
->>>>>>> 37504de9a8362bf5fb687c616be0b85b3435e898
-=======
->>>>>>> 9aef2f55f5e98510450522dac6de000556692de1
 import '../../styles/explorePage.css';
 import serverURL from '../../../utils/serverURL';
-import SearchIcon from '@material-ui/icons/Search';
-import DirectionsIcon from '@material-ui/icons/Directions';
-import callapi_explore_search from './callapi_explore/callapi_search' ;
-import Toolbar from '@material-ui/core/Toolbar';
-import AdvancedSearch from './component/advanced_search';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+
 import Like from "../likeContent/like";
 import Block from "../likeContent/block";
-import Typography from '@material-ui/core/Typography';
+
 import { withRouter } from 'react-router-dom';
 import Edit_profile from '../editProfile/editprofile';
 import SaveContent from "../saveContent/saveContent";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 9aef2f55f5e98510450522dac6de000556692de1
 import InfiniteScroll from 'react-infinite-scroll-component';
 import ShowMoreText from 'react-show-more-text';
 import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-<<<<<<< HEAD
-=======
-import LinearBuffer from './component/progress_bar_search';
->>>>>>> 37504de9a8362bf5fb687c616be0b85b3435e898
-=======
-import LinearBuffer from './component/progress_bar_search';
-
->>>>>>> 9aef2f55f5e98510450522dac6de000556692de1
 const drawerWidth = 240;
 
 
@@ -97,21 +63,6 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  appBar: {
-    // zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },  
   inputRoot: {
     color: 'inherit',
   },
@@ -140,14 +91,6 @@ function ExplorePage() {
   const [value, setValue] = useState(0);
   const [contentGet , setContentGet] = useState(false);
 
-  const [searched , setSearched] = useState("");
-  const [openAdvancedSearch , setOpenAdvancedSearch]   = useState(false);
-  const [searchMode , setSearchMode] = useState("") ;
-  const [searching , setSearching] = useState(false);
-  const [search , setSearch] = useState(false);
-
-  const [isSearched , setIsSearched ] = useState(false);
-
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -157,10 +100,6 @@ function ExplorePage() {
   };
 
   useEffect(async () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 9aef2f55f5e98510450522dac6de000556692de1
    
       if(contentGet == false)
       {
@@ -168,30 +107,10 @@ function ExplorePage() {
         setContentGet(true);
       }
     
-<<<<<<< HEAD
    
 
     // await timeout(20);
   }, []);
-=======
-    if(isSearched == false)
-=======
-     if(isSearched == false)
->>>>>>> 9aef2f55f5e98510450522dac6de000556692de1
-    {
-      fetchData();
-      setIsSearched(true);
-    }      
-
-<<<<<<< HEAD
-  });
->>>>>>> 37504de9a8362bf5fb687c616be0b85b3435e898
-=======
-    // await timeout(20);
-  }, []);
-
-
->>>>>>> 9aef2f55f5e98510450522dac6de000556692de1
 
   const fetchData = () => {
 
@@ -236,82 +155,9 @@ function ExplorePage() {
   { console.log(value + "valueeee") }
   return (
     <div>
-      <AdvancedSearch
-        open = {openAdvancedSearch}
-        handleChange = {(mode) => {
-          setSearchMode(mode)
-        }}
-        handleClose = {() => {
-          setOpenAdvancedSearch(false);
-        }}
-      />
-      <AppBar style={{ background: '#ffffff' }} position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-                        <Toolbar className="toolbar">
-                        <IconButton
-                            edge="start"
-                            color="inherit"
-                            aria-label="open drawer"
-                            onClick={handleDrawerOpen}
-                            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-                        >
-                            <MenuIcon />    
-                        </IconButton> 
-
-                        <Typography style={{flexGrow : '1'}} component="h1" variant="h6" color="inherit" noWrap className={classes.title}>                            
-                        </Typography>                                                          
-
-                        <Paper component="form" className='searchFormPaper'>                            
-                            <InputBase
-                              className={classes.input}
-                              placeholder="Search Content"
-                              // inputProps={{ 'aria-label': 'search' }}
-                              onChange = {(e) => {
-                                setSearched(e.target.value);
-                              }}
-                            />
-                            <IconButton onClick = {async () => {
-                                setSearching(true);
-                                const search_respoonse = await callapi_explore_search(searched  , searchMode);
-                                setSearching(false);
-                                setSearch(true);
-                                setContent((prevState) => ({                                  
-                                  items: search_respoonse.items,
-                                  total: search_respoonse.total
-                                }));
-                            }}  
-                             className={classes.iconButton} >
-                              <SearchIcon />
-                            </IconButton>
-                            <Divider className={classes.divider} orientation="vertical" />                            
-                          </Paper> 
-
-                          <IconButton onClick= {() => {
-                              setOpenAdvancedSearch(true);
-                          }} >
-                            <FilterListIcon />
-                          </IconButton >
-
-                          {search &&
-                          <Button className="Button" variant="contained" onClick={() => {
-                            fetchData();
-                            setSearch(false);                            
-                          }} color="secondary" autoFocus>
-                            cancel
-                          </Button> }
-
-                        <Typography style={{flexGrow : '1'}} component="h1" variant="h6" color="inherit" noWrap>                            
-                        </Typography>       
-                        </Toolbar>
-                    </AppBar>
-                    {searching && < LinearBuffer />}
-      <div style={{height : '60px'}} />
       <div className={classes.root}>
         <CssBaseline />
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 9aef2f55f5e98510450522dac6de000556692de1
 
         {/* <div className={classes.drawerHeader} /> */}
         <div className="explore">
@@ -330,17 +176,6 @@ function ExplorePage() {
             }
             endMessage={null
             }>
-<<<<<<< HEAD
-=======
-        
-          <div className={classes.drawerHeader} />
-
-          <div className="explore">
->>>>>>> 37504de9a8362bf5fb687c616be0b85b3435e898
-=======
-
-        
->>>>>>> 9aef2f55f5e98510450522dac6de000556692de1
             {content.length === 0 ? <div></div> :
               content.items.map((item,index) => {
                 if (item) return (
