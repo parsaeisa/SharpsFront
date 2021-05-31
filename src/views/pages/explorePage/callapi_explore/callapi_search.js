b38b9dfac624d3c81d11e10598456fa79c8a779a
Parsa Eisazadeh <p.eisazadeh@pinket.com>
2021-05-31 09:32:59 +0430

    feat ( search) : call api for searcb

--- src/views/pages/explorePage/callapi_explore/callapi_search.js
+++ src/views/pages/explorePage/callapi_explore/callapi_search.js
@@ -3,8 +3,15 @@ import serverURL from '../../../../utils/serverURL';
 import tokenConfig from  '../../../../utils/tokenConfig';
 
 export default function callapi_explore_search (searched , searchmode) {        
-            
-    const promise = axios.get(serverURL() + "content/search"  , tokenConfig());
+    
+    let mod = "";
+    if  (searchmode == 'URL')
+        mod = "$url=true" ;
+    if  (searchmode == 'title')
+        mod = "$title=true" ;
+
+    let query = searched + mod + "&skip=0&limit=10" ;    
+    const promise = axios.get(serverURL() + "content/search?s=" + query  , tokenConfig());
 
     const dataPromise = 
     promise.then((response) => response.data)    
